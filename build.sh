b8863ac4aeaa53b36c073bd3562392d241053466
MaZderMind <github@mazdermind.de>
2014-11-09 21:26:55 +0100

    experimental building script

--- build.sh
+++ build.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
+echo "building scss"
 scss assets/css/src/lustige-styles.scss assets/css/lustige-styles.css
 
-# spider html
-# copy assets
+echo "removing old build"
+rm -rf build
+
+echo "spidering page"
+mkdir -p build
+wget \
+	--no-verbose \
+	--no-clobber \
+	--no-host-directories \
+	--cut-dirs=3 \
+	--directory-prefix=build \
+	--user-agent=StaticBuildScript \
+	--recursive \
+	--no-parent \
+	--page-requisites \
+	--convert-links \
+	http://localhost/~peter/voc-frontends/31c3/
