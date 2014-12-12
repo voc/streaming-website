ebc241f23de5af08d55a826646102b0afb0e54d3
MaZderMind <github@mazdermind.de>
2014-12-12 01:01:22 +0100

    higher bitrates for party audio streams

--- lib/config.php
+++ lib/config.php
@@ -33,8 +33,12 @@ $GLOBALS['CONFIG']['FORMAT_TEXT'] = array(
 	'hd' =>   '1920x1080, h264+aac, 3 MBit/s',
 	'sd' =>   '1024x576, h264+aac, 1 MBit/s',
 	'webm' => '1024x576 vp8+vorbis in webm, 1 MBit/s',
+
 	'mp3' =>  'MP3, 128 kBit/s',
 	'opus' => 'Opus (RFC 6716), 96 kBit/s',
+
+	'party-mp3' =>  'MP3, 320 kBit/s',
+	'party-opus' => 'Opus (RFC 6716), 128 kBit/s',
 );
 
 // various room-name nappings
