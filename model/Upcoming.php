fd374f8b627bde637112cd90aee92e5c84301bbf
MaZderMind <github@mazdermind.de>
2015-04-06 15:03:38 +0200

    Show all upcoming Events in closed View

--- model/Upcoming.php
+++ model/Upcoming.php
@@ -2,14 +2,13 @@
 
 class Upcoming
 {
-	public function getNextEvent()
+	public function getNextEvents()
 	{
 		try {
 			$events = file_get_contents('https://c3voc.de/eventkalender/events.json?filter=upcoming');
 			$events = json_decode($events, true);
-			$names = array_keys($events['voc_events']);
 
-			return $events['voc_events'][$names[0]];
+			return array_values($events['voc_events']);
 		}
 		catch(ErrorException $e)
 		{
--- view/closed.php
+++ view/closed.php
@@ -1,10 +1,12 @@
 <?php
 
 $upcoming = new Upcoming();
+$events = $upcoming->getNextEvents();
 
 echo $tpl->render(array(
 	'page' => 'closed',
 	'title' => 'See you soon … somewhere else!',
 
-	'upcoming' => $upcoming->getNextEvent(),
+	'next' => @$events[0],
+	'events' => $events,
 ));
