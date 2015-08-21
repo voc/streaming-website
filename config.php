c1bc9863cfbf1f4c3d92b77cc33913a8e7ba61dd
MaZderMind <github@mazdermind.de>
2015-08-21 12:21:54 +0200

    Set new default VOD-Url

--- config.php
+++ config.php
@@ -91,7 +91,7 @@ $GLOBALS['CONFIG']['CONFERENCE'] = array(
 	 * Wird beides aktiviert, hat der externe Link Vorrang!
 	 * Wird beides auskommentiert, wird der Link nicht angezeigt
 	 */
-	'RELIVE_JSON' => 'http://vod.c3voc.de/index.json',
+	'RELIVE_JSON' => 'http://cdn.c3voc.de/releases/relive/index.json',
 
 	/**
 	 * APCU-Cache-Zeit in Sekunden
