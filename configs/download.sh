b893ae448ea9661554608f097457042d7b0793d7
v0tti <alex@alexandervotteler.de>
2015-08-29 16:11:06 +0200

    Initial commit for MRMCD15
    Vielleicht guckt sich jemand mit Ahnung die config.php noch mal an

--- configs/download.sh
+++ configs/download.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # fahrplan
-wget -q "http://events.ccc.de/congress/2014/Fahrplan/schedule.xml" -O /tmp/schedule.xml && mv /tmp/schedule.xml schedule.xml
+wget -q "http://mrmcd.net/2015/fahrplan/schedule.xml" -O /tmp/schedule.xml && mv /tmp/schedule.xml schedule.xml
 
 # vod json
 wget -q "http://cdn.c3voc.de/releases/relive/index.json" -O /tmp/vod.json && mv /tmp/vod.json vod.json
