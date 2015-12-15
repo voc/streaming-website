a35d75c584b3c9647be3528f0b0d38ea0550af1d
MaZderMind <github@mazdermind.de>
2015-12-15 11:22:50 +0100

    [32c3] update vod-json url

--- configs/conferences/32c3/download.sh
+++ configs/conferences/32c3/download.sh
@@ -4,4 +4,4 @@
 wget --no-check-certificate -q "http://data.c3voc.de/32C3/everything.schedule.xml" -O /tmp/schedule.xml && mv /tmp/schedule.xml schedule.xml
 
 # vod json
-wget -q "http://cdn.c3voc.de/releases/relive/index.json" -O /tmp/vod.json && mv /tmp/vod.json vod.json
+wget -q "http://cdn.c3voc.de/relive/index.json" -O /tmp/vod.json && mv /tmp/vod.json vod.json
