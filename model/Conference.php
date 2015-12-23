0ed383670ed0d18b343ab578eca90084e07d1dfc
MaZderMind <github@mazdermind.de>
2015-12-23 14:12:10 +0100

    use SERVER_NAME everywhere

--- model/Conference.php
+++ model/Conference.php
@@ -7,7 +7,7 @@ class Conference extends ModelBase
 	}
 
 	public function isPreviewEnabled() {
-		return $this->has('PREVIEW_DOMAIN') && ($this->get('PREVIEW_DOMAIN') == $_SERVER['HTTP_HOST']);
+		return $this->has('PREVIEW_DOMAIN') && ($this->get('PREVIEW_DOMAIN') == $_SERVER['SERVER_NAME']);
 	}
 
 	public function isClosed() {
