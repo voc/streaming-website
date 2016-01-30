1b4b3526d83316406c896dccfc93b6c5c4c27827
MaZderMind <git@mazdermind.de>
2016-01-30 23:08:35 +0100

    relive lives on live.ber

--- configs/conferences/ffrl-routing-days-2016/download.sh
+++ configs/conferences/ffrl-routing-days-2016/download.sh
@@ -5,5 +5,5 @@ wget --no-check-certificate -q "https://mlsrv.de/public/routingdays16-schedule/s
 rm -f /tmp/schedule.xml /tmp/schedule.json
 
 # vod json
-wget -q "http://live.dus.c3voc.de/relive/ffrl-routing-days/index.json" -O /tmp/vod.json && mv /tmp/vod.json vod.json
+wget -q "http://live.ber.c3voc.de/releases/relive/ffrl-routing-days-2016/ffrl-routing-days/index.json" -O /tmp/vod.json && mv /tmp/vod.json vod.json
 rm -f /tmp/vod.json
