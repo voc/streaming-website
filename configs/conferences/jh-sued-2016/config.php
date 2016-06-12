2a6a9b0632bfc416ab0133f0ac7655a48edf9261
thomic <pirat.thomic@googlemail.com>
2016-06-12 03:16:18 +0200

    TEST for description

--- configs/conferences/jh-sued-2016/config.php
+++ configs/conferences/jh-sued-2016/config.php
@@ -47,7 +47,7 @@ $GLOBALS['CONFIG']['CONFERENCE'] = array(
 	 * Wird für den <meta name="description">-Tag verdet. Wird diese Zeile auskommentiert, wird kein solcher
 	 * <meta>-Tag generiert.
 	 */
-//	'DESCRIPTION' => '',
+	'DESCRIPTION' => 'TEST1234',
 
 	/**
 	 * Schlüsselwortliste, Kommasepariert
