f6799b2363435e26a0d46be81a7e03327d101ef7
Florian Larysch <fl@n621.de>
2016-08-11 22:13:29 +0200

    guadec2016: fix start date

--- configs/conferences/guadec2016/config.php
+++ configs/conferences/guadec2016/config.php
@@ -8,7 +8,7 @@ $GLOBALS['CONFIG']['CONFERENCE'] = array(
 	 * Wird dieser Zeitpunkt nicht angegeben, gilt die Konferenz immer als angefangen. (Siehe aber ENDS_AT
 	 * und CLOSED weiter unten)
 	 */
-	'STARTS_AT' => strtotime("2016-08-11 08:00"),
+	'STARTS_AT' => strtotime("2016-08-12 08:00"),
 
 	/**
 	 * Der Endzeitpunkt der Konferenz als Unix-Timestamp. Befinden wir uns danach, wird eine Danke-Und-Kommen-Sie-
