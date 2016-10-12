ade5db5f8280cd356ece3d9a59e6a56f00794391
Peter Körner <pkoerner@seibert-media.net>
2016-10-12 09:35:01 +0200

    lint php-files before deploying

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+
+find . -name "*.php" -print0 | xargs -0 -n1 php -l
+if [ $? -ne 0 ]; then
+	echo "not deploying b0rken code ;)"
+	exit 1
+fi
+
 ssh voc@lb.dus.c3voc.de 'sudo sh' << EOT
 cd /srv/nginx/streaming-website
 git fetch origin
