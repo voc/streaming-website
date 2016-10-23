d354441589d4bc88963bafc7d96187d4b0d2df75
VOC User <voc@streaming.media.ccc.de>
2016-10-23 23:31:01 +0200

    fixed typo in dowload.sh

--- configs/conferences/pw16/download.sh
+++ configs/conferences/pw16/download.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # fahrplan
-wget --no-check-certificate -q "https://fahrplan.privacyweek.at/schedule.xml" -O /tmp/pw16-schedule.xml && mv /tmp/pw-schedule.xml schedule.xml
+wget --no-check-certificate -q "https://fahrplan.privacyweek.at/schedule.xml" -O /tmp/pw16-schedule.xml && mv /tmp/pw16-schedule.xml schedule.xml
 
 # relive
 wget -q "http://live.dus.c3voc.de/relive/pw16/index.json" -O /tmp/vod.json && mv /tmp/vod.json vod.json
