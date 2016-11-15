9f1746a9eb4d9734bddd41e9a449ba2bed1eca0a
Peter Hasse <peter.hasse@fokus.fraunhofer.de>
2016-11-15 16:59:52 +0100

    added check for depencies to deploy.sd

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+command -v find >/dev/null 2>&1 || { echo >&2 "I require find but it's not installed.  Aborting."; exit 1; }
+command -v xargs >/dev/null 2>&1 || { echo >&2 "I require xargs but it's not installed.  Aborting."; exit 1; }
+command -v php >/dev/null 2>&1 || { echo >&2 "I require php but it's not installed.  Aborting."; exit 1; }
+
 find . -name "*.php" -print0 | xargs -0 -n1 php -l
 if [ $? -ne 0 ]; then
 	echo "not deploying b0rken code ;)"
