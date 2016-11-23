1a1055ef62ea7315cb1ae1ec608bb42d23ff4f89
Daniel Molkentin <danimo@owncloud.com>
2016-11-23 13:22:43 +0100

    Fix schedule

--- configs/conferences/denog16/config.php
+++ configs/conferences/denog16/config.php
@@ -231,7 +231,7 @@ $GLOBALS['CONFIG']['ROOMS'] = array(
 		 * Name des Raums im Fahrplan
 		 * Wenn diese Zeile auskommentiert ist wird der Raum-Slug verwendet
 		 */
-		//'SCHEDULE_NAME' => 'Saal 1',
+		'SCHEDULE_NAME' => 'darmstadtium',
 
 		/**
 		 * Feedback anzeigen (boolean)
