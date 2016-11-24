75641818b5068d41e2a04b4286e74e0b8cc9f54c
Daniel Meißner <meise@koeln.ccc.de>
2016-11-24 22:52:57 +0100

    fixed download script

--- configs/conferences/fiffkon2016/download.sh
+++ configs/conferences/fiffkon2016/download.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # fahrplan
-wget --no-check-certificate -q "https://datenspuren.de/2016/fahrplan/schedule.xml" -O /tmp/DS2016-schedule.xml && mv /tmp/DS2016-schedule.xml schedule.xml
+wget --no-check-certificate -q "http://data.testi.ber.c3voc.de/schedule/fiffkon16/schedule-Hoersaal.xml" -O /tmp/fiffkon16-schedule.xml && mv /tmp/fiffkon-schedule.xml schedule.xml
 
 # relive
-wget -q "http://live.dus.c3voc.de/relive/DS2016/index.json" -O /tmp/vod.json && mv /tmp/vod.json vod.json
+wget -q "http://live.dus.c3voc.de/relive/fiffkon/index.json" -O /tmp/vod.json && mv /tmp/vod.json vod.json
 rm -f /tmp/vod.json
