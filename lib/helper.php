dbed89499b93b2ec3c2c50e5805ef8393a8ef990
MaZderMind <git@mazdermind.de>
2016-12-21 18:22:37 +0100

    work on startpage style

--- lib/helper.php
+++ lib/helper.php
@@ -145,3 +145,30 @@ function array_filter_last($array, $predicate)
 
 	return null;
 }
+
+function slugify($text)
+{
+	// replace non letter or digits by -
+	$text = preg_replace('~[^\pL\d]+~u', '-', $text);
+
+	// transliterate
+	$text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+	// remove unwanted characters
+	$text = preg_replace('~[^-\w]+~', '', $text);
+
+	// trim
+	$text = trim($text, '-');
+
+	// remove duplicate -
+	$text = preg_replace('~-+~', '-', $text);
+
+	// lowercase
+	$text = strtolower($text);
+
+	if (empty($text)) {
+		return 'none';
+	}
+
+	return $text;
+}
