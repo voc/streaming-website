56a2420e47f9434b4c91b17e81a36fb4e6fe887d
Florian Larysch <fl@n621.de>
2017-01-03 16:36:33 +0100

    relive: handle missing scrub thumbs correctly
    Previously, when relive didn't supply a sprites file to the player, we'd
    simply pass undefined into buildSpriteConfig, which only worked because
    of the way the arguments are handled internally in that function, but
    does not represent a proper use of the plugin's API.
    
    Instead, we now manually check whether relive has supplied a sprites
    file and explicitly pass an empty thumbs array to the plugin in such a
    case.

--- assets/js/lustiges-script.js
+++ assets/js/lustiges-script.js
@@ -21,6 +21,18 @@ $(function() {
 
 	var $relivePlayer = $('body.relive-player .video-wrap');
 	if($relivePlayer.length > 0) {
+		var sprites = [];
+
+		if($relivePlayer.data("sprites")) {
+			sprites = ClapprThumbnailsPlugin.buildSpriteConfig(
+				$relivePlayer.data("sprites"),
+				$relivePlayer.data("sprites-n"),
+				160, 90,
+				$relivePlayer.data("sprites-cols"),
+				$relivePlayer.data("sprites-interval")
+			);
+		}
+
 		var player = new Clappr.Player({
 			baseUrl: 'assets/clapprio/',
 			plugins: {
@@ -34,13 +46,7 @@ $(function() {
 			scrubThumbnails: {
 				backdropHeight: 64,
 				spotlightHeight: 84,
-				thumbs: ClapprThumbnailsPlugin.buildSpriteConfig(
-					$relivePlayer.data("sprites"),
-					$relivePlayer.data("sprites-n"),
-					160, 90,
-					$relivePlayer.data("sprites-cols"),
-					$relivePlayer.data("sprites-interval")
-				),
+				thumbs: sprites
 			},
 			events: {
 				onReady: function() {
