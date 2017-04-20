5f008f47fd288353bd651a1e168ff683705e3051
Daniel Meißner <meise@koeln.ccc.de>
2017-04-20 19:29:37 +0200

    osmocon: activated relive

--- configs/conferences/osmocon17/config.php
+++ configs/conferences/osmocon17/config.php
@@ -91,7 +91,7 @@ $CONFIG['CONFERENCE'] = array(
 	 *
 	 * Wird diese Zeile auskommentiert, wird der Link nicht angezeigt
 	 */
-	//'RELIVE_JSON' => 'https://live.dus.c3voc.de/relive/33c3/index.json',
+	 'RELIVE_JSON' => 'http://live.ber.c3voc.de/relive/osmocon17/index.json',
 );
 
 /**
