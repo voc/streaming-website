7a5223ec0258ce00ba78a05f1570bb11a6a45aca
Daniel Meißner <meise@koeln.ccc.de>
2017-04-29 11:16:56 +0200

    sha orga meeting 2904: fixed end date

--- configs/conferences/sha2017orgameeting20170429/config.php
+++ configs/conferences/sha2017orgameeting20170429/config.php
@@ -16,7 +16,7 @@ $CONFIG['CONFERENCE'] = array(
 	 *
 	 * Wird dieser Zeitpunkt nicht angegeben, endet die Konferenz nie. (Siehe aber CLOSED weiter unten)
 	 */
-	'ENDS_AT' => strtotime("2017-04-28 16:00"),
+	'ENDS_AT' => strtotime("2017-04-29 16:00"),
 
 	/**
 	 * Hiermit kann die Funktionalitaet von STARTS_AT/ENDS_AT überschrieben werden. Der Wert 'before'
