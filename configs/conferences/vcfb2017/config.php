1baf6b8f9d755ad0694f1efc70701525cc924a7d
Peter Hasse <peter.hasse@fokus.fraunhofer.de>
2017-10-06 12:00:50 +0200

    added schedule url removed old download.sh

--- configs/conferences/vcfb2017/config.php
+++ configs/conferences/vcfb2017/config.php
@@ -364,7 +364,7 @@ $CONFIG['SCHEDULE'] = array(
 	 * aufhören zu funktionieren. Wenn die Quelle unverlässlich ist ;) sollte ein
 	 * externer HTTP-Cache vorgeschaltet werden.
 	 */
-	'URL' => 'http://geruempel.ddns.net/schedule_voc.xml',
+	'URL' => 'http://vcfb.de/2017/schedule.xml',
 
 	/**
 	* Nur die angegebenen Räume aus dem Fahrplan beachten
--- configs/conferences/vcfb2017/download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# fahrplan
-wget --no-check-certificate -q "geruempel.ddns.net/schedule_voc.xml" -O /tmp/vcfb2016-schedule.xml && mv /tmp/vcfb2016-schedule.xml schedule.xml
-rm -f /tmp/vcfb2016-schedule.xml
-
-# relive
-wget -q "http://live.dus.c3voc.de/relive/vcfb2016/index.json" -O /tmp/vcfb2016-vod.json && mv /tmp/vcfb2016-vod.json vod.json
-rm -f /tmp/vcfb2016-vod.json
