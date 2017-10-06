325941826a0d75f75340de73b3b0dd892717823e
Daniel Meißner <daniel@3st.be>
2017-10-06 19:30:29 +0200

    vcfb2017: fixed room overview

--- configs/conferences/vcfb2017/config.php
+++ configs/conferences/vcfb2017/config.php
@@ -136,7 +136,7 @@ $CONFIG['ROOMS'] = array(
 	 * Array-Key ist der Raum-Slug, der z.B. auch zum erstellen der URLs,
 	 * in $CONFIG['OVERVIEW'] oder im Feedback verwendet wird.
 	 */
-	'Medientheater' => array(
+	'Bildungsraum' => array(
 		/**
 		 * Angezeige-Name
 		 */
