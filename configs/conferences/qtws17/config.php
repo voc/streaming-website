a3094daedced2f362311959fdb1dcd52d0eaa2ae
Daniel Meißner <daniel@3st.be>
2017-10-11 15:17:35 +0200

    qtws17: next try to close conference

--- configs/conferences/qtws17/config.php
+++ configs/conferences/qtws17/config.php
@@ -8,7 +8,7 @@ $CONFIG['CONFERENCE'] = array(
 	 * Wird dieser Zeitpunkt nicht angegeben, gilt die Konferenz immer als angefangen. (Siehe aber ENDS_AT
 	 * und CLOSED weiter unten)
 	 */
-	'STARTS_AT' => strtotime("2017-10-11 08:55"),
+	'STARTS_AT' => strtotime("2017-10-12 08:55"),
 
 	/**
 	 * Der Endzeitpunkt der Konferenz als Unix-Timestamp. Befinden wir uns danach, wird eine Danke-Und-Kommen-Sie-
@@ -16,7 +16,7 @@ $CONFIG['CONFERENCE'] = array(
 	 *
 	 * Wird dieser Zeitpunkt nicht angegeben, endet die Konferenz nie. (Siehe aber CLOSED weiter unten)
 	 */
-	'ENDS_AT' => strtotime("2017-10-12 12:45"),
+	'ENDS_AT' => strtotime("2017-10-12 14:00"),
 
 	/**
 	 * Hiermit kann die Funktionalitaet von STARTS_AT/ENDS_AT überschrieben werden. Der Wert 'before'
