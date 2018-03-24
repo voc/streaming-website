ab6b41fcf3503ff0701e98ac986b4da5a86def7f
MaZderMind <git@mazdermind.de>
2018-03-24 16:37:34 +0100

    extend timeline right by 3 hours

--- configs/conferences/fossgis18/config.php
+++ configs/conferences/fossgis18/config.php
@@ -16,7 +16,7 @@ $CONFIG['CONFERENCE'] = array(
 	 *
 	 * Wird dieser Zeitpunkt nicht angegeben, endet die Konferenz nie. (Siehe aber CLOSED weiter unten)
 	 */
-	'ENDS_AT' => strtotime("2018-03-24 15:00"),
+	'ENDS_AT' => strtotime("2018-03-24 18:00"),
 
 	/**
 	 * Hiermit kann die Funktionalitaet von STARTS_AT/ENDS_AT überschrieben werden. Der Wert 'before'
