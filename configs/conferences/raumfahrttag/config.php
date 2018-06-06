ee4816a760b83003b821d327f96cfab6333d44f8
derpeter <sites@derpeter.net>
2018-06-06 07:43:47 +0200

    updated start time raumfahrttag

--- configs/conferences/raumfahrttag/config.php
+++ configs/conferences/raumfahrttag/config.php
@@ -8,7 +8,7 @@ $CONFIG['CONFERENCE'] = array(
 	 * Wird dieser Zeitpunkt nicht angegeben, gilt die Konferenz immer als angefangen. (Siehe aber ENDS_AT
 	 * und CLOSED weiter unten)
 	 */
-	'STARTS_AT' => strtotime("2018-06-06 11:30"),
+	'STARTS_AT' => strtotime("2018-06-06 11:00"),
 
 	/**
 	 * Der Endzeitpunkt der Konferenz als Unix-Timestamp. Befinden wir uns danach, wird eine Danke-Und-Kommen-Sie-
