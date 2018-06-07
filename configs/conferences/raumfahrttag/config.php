dca45594be40fadccfe19c8c2818aaeb2915fb71
derpeter <sites@derpeter.net>
2018-06-07 09:24:57 +0200

    fixed wrong end dates

--- configs/conferences/raumfahrttag/config.php
+++ configs/conferences/raumfahrttag/config.php
@@ -16,7 +16,7 @@ $CONFIG['CONFERENCE'] = array(
 	 *
 	 * Wird dieser Zeitpunkt nicht angegeben, endet die Konferenz nie. (Siehe aber CLOSED weiter unten)
 	 */
-	'ENDS_AT' => strtotime("2018-11-26 18:00"),
+	'ENDS_AT' => strtotime("2018-06-06 18:00"),
 
 	/**
 	 * Hiermit kann die Funktionalitaet von STARTS_AT/ENDS_AT überschrieben werden. Der Wert 'before'
