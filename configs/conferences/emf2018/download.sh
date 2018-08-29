9555d03aed24e3778241f9923b8876c1ea7c13a4
v0tti <alex@alexandervotteler.de>
2018-08-29 12:17:13 +0200

    emf2018

---
+++ configs/conferences/emf2018/download.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# fahrplan
+wget --no-check-certificate -q "https://www.emfcamp.org/schedule.frab" -O /tmp/emf2016-schedule.xml && mv /tmp/emf2016-schedule.xml schedule.xml
+rm -f /tmp/emf2016-schedule.xml
+
+# relive
+#wget -q "http://live.dus.c3voc.de/relive/emf2016/index.json" -O /tmp/emf2016-vod.json && mv /tmp/emf2016-vod.json vod.json
+#rm -f /tmp/emf2016-vod.json
