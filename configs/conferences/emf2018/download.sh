b6fcc301bd9de6d5c44ffee93930d88f748da0f4
v0tti <alex@alexandervotteler.de>
2018-08-30 12:07:20 +0200

    correct fahrplan paths

--- configs/conferences/emf2018/download.sh
+++ configs/conferences/emf2018/download.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # fahrplan
-wget --no-check-certificate -q "https://www.emfcamp.org/schedule.frab" -O /tmp/emf2016-schedule.xml && mv /tmp/emf2016-schedule.xml schedule.xml
-rm -f /tmp/emf2016-schedule.xml
+wget --no-check-certificate -q "https://www.emfcamp.org/schedule.frab" -O /tmp/emf2018-schedule.xml && mv /tmp/emf2018-schedule.xml schedule.xml
+rm -f /tmp/emf2018-schedule.xml
 
 # relive
 #wget -q "http://live.dus.c3voc.de/relive/emf2016/index.json" -O /tmp/emf2016-vod.json && mv /tmp/emf2016-vod.json vod.json
