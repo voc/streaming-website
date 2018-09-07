e644f70079e4dfdf9463bba399d04ff868ca02fd
derchris <derchris@me.com>
2018-09-07 16:46:05 +0200

    mrmcd: change time

--- configs/conferences/mrmcd18/config.php
+++ configs/conferences/mrmcd18/config.php
@@ -8,7 +8,7 @@ $CONFIG['CONFERENCE'] = array(
 	 * Wird dieser Zeitpunkt nicht angegeben, gilt die Konferenz immer als angefangen. (Siehe aber ENDS_AT
 	 * und CLOSED weiter unten)
 	 */
-	'STARTS_AT' => strtotime("2018-09-07 16:00"),
+	'STARTS_AT' => strtotime("2018-09-07 15:00"),
 
 	/**
 	 * Der Endzeitpunkt der Konferenz als Unix-Timestamp. Befinden wir uns danach, wird eine Danke-Und-Kommen-Sie-
