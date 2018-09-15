c5c063a12789d209d51c0f29a8abb66377894454
Daniel Molkentin <dmolkentin@suse.com>
2018-09-15 22:57:32 +0200

    suselabs18: correct relive URL now that its armed

--- configs/conferences/suselabs18/config.php
+++ configs/conferences/suselabs18/config.php
@@ -96,7 +96,7 @@ $CONFIG['CONFERENCE'] = array(
 	 * Wird beides aktiviert, hat der externe Link Vorrang!
 	 * Wird beides auskommentiert, wird der Link nicht angezeigt
 	 */
-	'RELIVE_JSON' => 'http://live.ber.c3voc.de/relive/oSC18/index.json',
+	'RELIVE_JSON' => 'http://live.ber.c3voc.de/relive/suselabs18/index.json',
 
 	/**
 	 * APCU-Cache-Zeit in Sekunden
