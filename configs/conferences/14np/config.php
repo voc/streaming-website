b0e885a7c1bade7047ed649992ee7c5815eff6a2
Peter Hasse <peter.hasse@fokus.fraunhofer.de>
2018-09-21 10:20:02 +0200

    fixed date

--- configs/conferences/14np/config.php
+++ configs/conferences/14np/config.php
@@ -16,7 +16,7 @@ $CONFIG['CONFERENCE'] = array(
 	 *
 	 * Wird dieser Zeitpunkt nicht angegeben, endet die Konferenz nie. (Siehe aber CLOSED weiter unten)
 	 */
-	'ENDS_AT' => strtotime("2017-09-21 19:00"),
+	'ENDS_AT' => strtotime("2018-09-21 19:00"),
 
 	/**
 	 * Hiermit kann die Funktionalitaet von STARTS_AT/ENDS_AT überschrieben werden. Der Wert 'before'
