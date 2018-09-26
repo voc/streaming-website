8ea19152c9eb163b331bf59e4267a0883f67b626
Daniel Molkentin <daniel@molkentin.de>
2018-09-26 22:36:14 +0200

    asg2018 will use s1+s2

--- configs/conferences/asg2018/config.php
+++ configs/conferences/asg2018/config.php
@@ -146,7 +146,7 @@ $CONFIG['ROOMS'] = array(
 		 * Event-Case in welchem Raum aufgebaut wird und wird üblicherweise von
 		 * s1 bis s5 durchnummeriert.
 		 */
-		'STREAM' => 's4',
+		'STREAM' => 's1',
 
 		/**
 		 * Stream-Vorschaubildchen auf der Übersichtsseite anzeigen
@@ -344,7 +344,7 @@ $CONFIG['ROOMS'] = array(
 
   	'kuppel' => array(
   		'DISPLAY' => 'Kuppel',
-  		'STREAM' => 's4',
+  		'STREAM' => 's2',
   		'PREVIEW' => true,
   
   		'TRANSLATION' => false,
