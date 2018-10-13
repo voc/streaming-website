3c626d00326e290a16141d6bb164277b3156720d
Daniel Molkentin <daniel@molkentin.de>
2018-10-13 16:12:21 +0200

    vcfb2018: fix relive url

--- configs/conferences/vcfb2018/config.php
+++ configs/conferences/vcfb2018/config.php
@@ -98,7 +98,7 @@ Das Vintage Computing Festival Berlin (VCFB) ist eine Veranstaltung rund um hist
 	 * Wird beides aktiviert, hat der externe Link Vorrang!
 	 * Wird beides auskommentiert, wird der Link nicht angezeigt
 	 */
-	'RELIVE_JSON' => 'http://live.ber.c3voc.de/relive/vcfb18/index.json',
+	'RELIVE_JSON' => 'http://live.ber.c3voc.de/relive/vcfb2018/index.json',
 
 	/**
 	 * APCU-Cache-Zeit in Sekunden
