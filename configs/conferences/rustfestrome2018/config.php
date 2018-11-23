c360d2562d24dcf919885bb6033196ff57fa2a0e
Sophie Schiller <sophie.schiller01@gmail.com>
2018-11-24 00:27:45 +0100

    rustfestrome2018: set correct room

--- configs/conferences/rustfestrome2018/config.php
+++ configs/conferences/rustfestrome2018/config.php
@@ -146,7 +146,7 @@ $CONFIG['ROOMS'] = array(
 		 * Event-Case in welchem Raum aufgebaut wird und wird üblicherweise von
 		 * s1 bis s5 durchnummeriert.
 		 */
-		'STREAM' => 's6',
+		'STREAM' => 's2',
 
 		/**
 		 * Stream-Vorschaubildchen auf der Übersichtsseite anzeigen
