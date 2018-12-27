78ad494637c1b6faad4cda1c5ac74b018be877dc
dedeibel <dedeibel@arcor.de>
2018-12-27 22:06:17 +0100

    Request feedback login

--- model/Feedback.php
+++ model/Feedback.php
@@ -54,7 +54,6 @@ class Feedback
 
 	public function isLoggedIn()
 	{
-		return true;
 		return
 			isset($_SERVER['PHP_AUTH_USER']) &&
 			$_SERVER['PHP_AUTH_USER'] == $this->getConference()->get('FEEDBACK.USERNAME') &&
