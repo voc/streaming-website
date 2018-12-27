0af34da3dc28495f63e94d9e25d3df040b365333
dedeibel <dedeibel@arcor.de>
2018-12-27 15:18:16 +0100

    35c3: relive url geändert

--- configs/conferences/35c3/config.php
+++ configs/conferences/35c3/config.php
@@ -108,7 +108,7 @@ $CONFIG['CONFERENCE'] = array(
 	 *
 	 * Wird diese Zeile auskommentiert, wird der Link nicht angezeigt
 	 */
-	'RELIVE_JSON' => 'https://live.ber.c3voc.de/relive/35c3/index.json',
+	'RELIVE_JSON' => 'https://cdn.c3voc.de/relive/35c3/index.json',
 );
 
 /**
