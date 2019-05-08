308d1e278102a6dc50a3ad539cedcbaf2966365d
Peter Hasse <peter.hasse@fokus.fraunhofer.de>
2019-05-08 10:12:38 +0200

    danimo | derpeter: besser mit nullen

--- configs/conferences/fusion-presscon/config.php
+++ configs/conferences/fusion-presscon/config.php
@@ -8,7 +8,7 @@ $CONFIG['CONFERENCE'] = array(
 	 * Wird dieser Zeitpunkt nicht angegeben, gilt die Konferenz immer als angefangen. (Siehe aber ENDS_AT
 	 * und CLOSED weiter unten)
 	 */
-	'STARTS_AT' => strtotime("2019-5-8 14:00"),
+	'STARTS_AT' => strtotime("2019-05-08 14:00"),
 
 	/**
 	 * Der Endzeitpunkt der Konferenz als Unix-Timestamp. Befinden wir uns danach, wird eine Danke-Und-Kommen-Sie-
@@ -16,7 +16,7 @@ $CONFIG['CONFERENCE'] = array(
 	 *
 	 * Wird dieser Zeitpunkt nicht angegeben, endet die Konferenz nie. (Siehe aber CLOSED weiter unten)
 	 */
-	'ENDS_AT' => strtotime("2019-5-8 17:00"),
+	'ENDS_AT' => strtotime("2019-05-08 17:00"),
 
 	/**
 	 * Hiermit kann die Funktionalitaet von STARTS_AT/ENDS_AT überschrieben werden. Der Wert 'before'
