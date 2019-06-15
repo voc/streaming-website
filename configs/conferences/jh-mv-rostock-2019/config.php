9b9aca048f0604e12a7d842c19ec323afe62f3ee
Daniel Molkentin <daniel@molkentin.de>
2019-06-15 12:57:42 +0200

    open even earlier

--- configs/conferences/jh-mv-rostock-2019/config.php
+++ configs/conferences/jh-mv-rostock-2019/config.php
@@ -8,7 +8,7 @@ $CONFIG['CONFERENCE'] = array(
 	 * Wird dieser Zeitpunkt nicht angegeben, gilt die Konferenz immer als angefangen. (Siehe aber ENDS_AT
 	 * und CLOSED weiter unten)
 	 */
-	'STARTS_AT' => strtotime("2019-06-15 14:00"),
+	'STARTS_AT' => strtotime("2019-06-15 13:00"),
 
 	/**
 	 * Der Endzeitpunkt der Konferenz als Unix-Timestamp. Befinden wir uns danach, wird eine Danke-Und-Kommen-Sie-
