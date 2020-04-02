ba7e09cbf039bc62b6d879a30c3f057b652da19b
derchris <derchris@me.com>
2020-04-02 14:39:57 +0200

    debate12: use cccb setup

--- configs/conferences/debate12/config.php
+++ configs/conferences/debate12/config.php
@@ -154,7 +154,7 @@ $CONFIG['ROOMS'] = array(
 		 * Event-Case in welchem Raum aufgebaut wird und wird üblicherweise von
 		 * s1 bis s5 durchnummeriert.
 		 */
-		'STREAM' => 's5',
+		'STREAM' => 's23',
 
 		/**
 		 * Stream-Vorschaubildchen auf der Übersichtsseite anzeigen
