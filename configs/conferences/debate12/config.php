e212526e7ece0ab2dd263164d39711f1b6ef8b9e
derchris <derchris@me.com>
2020-04-02 14:33:54 +0200

    debate12: add irc details

--- configs/conferences/debate12/config.php
+++ configs/conferences/debate12/config.php
@@ -425,4 +425,26 @@ $CONFIG['TWITTER'] = array(
 	'TEXT' => '#debate',
 );
 
+$CONFIG['IRC'] = array(
+    /**
+     * Anzeigetext für die IRC-Links.
+     *
+     * %s wird durch den Raum-Slug ersetzt.
+     * Ist eine weitere Anpassung erfoderlich, kann ein IRC_CONFIG-Block in der
+     * Raum-Konfiguration zum Überschreiben dieser Angaben verwendet werden.
+     */
+    'DISPLAY' => '#debate @ hackint',
+
+    /**
+     * URL für die IRC-Links.
+     * Hierbei kann sowohl ein irc://-Link als auch ein Link zu einem
+     * WebIrc-Provider wie z.B. 'https://kiwiirc.com/client/irc.hackint.eu/#33C3-%s'
+     * verwendet werden.
+     *
+     * %s wird durch den urlencodeten Raum-Slug ersetzt.
+     * Eine Anpassung kann ebenfalls in der Raum-Konfiguration vorgenommen werden.
+     */
+    'URL' => 'https://webirc.hackint.org/#irc://irc.hackint.org/#debate',
+);
+
 return $CONFIG;
