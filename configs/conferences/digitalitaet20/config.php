5e33c9a0bb6848019c64bf12706aaea621d3f4d3
Daniel Molkentin <dmolkentin@suse.com>
2020-06-12 18:41:37 +0200

    Add digitalitaet20

---
+++ configs/conferences/digitalitaet20/config.php
@@ -0,0 +1,390 @@
+<?php
+
+$CONFIG['CONFERENCE'] = array(
+	/**
+	 * Der Startzeitpunkt der Konferenz als Unix-Timestamp. Befinden wir uns davor, wird die Closed-Seite
+	 * mit einem Text der Art "hat noch nicht angefangen" angezeigt.
+	 *
+	 * Wird dieser Zeitpunkt nicht angegeben, gilt die Konferenz immer als angefangen. (Siehe aber ENDS_AT
+	 * und CLOSED weiter unten)
+	 */
+	'STARTS_AT' => strtotime("2020-06-20 10:00"),
+
+	/**
+	 * Der Endzeitpunkt der Konferenz als Unix-Timestamp. Befinden wir uns danach, wird eine Danke-Und-Kommen-Sie-
+	 * Gut-Nach-Hause-Seite sowie einem Ausblick auf die kommenden Events angezeigt.
+	 *
+	 * Wird dieser Zeitpunkt nicht angegeben, endet die Konferenz nie. (Siehe aber CLOSED weiter unten)
+	 */
+	'ENDS_AT' => strtotime("2020-06-20 16:30"),
+
+	/**
+	 * Hiermit kann die Funktionalitaet von STARTS_AT/ENDS_AT überschrieben werden. Der Wert 'before'
+	 * simuliert, dass die Konferenz noch nicht begonnen hat. Der Wert 'after' simuliert, dass die Konferenz
+	 * bereits beendet ist. 'running' simuliert eine laufende Konferenz.
+	 *
+	 * Der Boolean true ist aus Abwärtskompatibilitätsgründen äquivalent zu 'after'. False ist äquivalent
+	 * zu 'running'.
+	 */
+	'CLOSED' => 'true',
+
+	/**
+	 * Titel der Konferenz (kann Leer- und Sonderzeichen enthalten)
+	 * Dieser im Seiten-Header, im <title>-Tag, in der About-Seite und ggf. ab weiteren Stellen als
+	 * Anzeigetext benutzt
+	 */
+	'TITLE' => 'Digitalität 20 - #Schuleneudenken',
+
+	/**
+	 * Veranstalter
+	 * Wird für den <meta name="author">-Tag verdet. Wird diese Zeile auskommentiert, wird kein solcher
+	 * <meta>-Tag generiert.
+	 */
+	'AUTHOR' => 'C3VOC',
+
+	/**
+	 * Beschreibungstext
+	 * Wird für den <meta name="description">-Tag verdet. Wird diese Zeile auskommentiert, wird kein solcher
+	 * <meta>-Tag generiert.
+	 */
+	'DESCRIPTION' => '#Schuleneudenken – das ist ein Auftrag an uns alle! Wir haben jetzt die Chance, Veränderungen an Schulen anzustoßen, um zeitgemäße Bildung in einer Kultur der Digitalität zu ermöglichen.
+Diskutiere im Barcamp mit namhaften Vertretern aus Schule, Wissenschaft und Wirtschaft über Konzepte und Ideen für eine Schule der Zukunft.',
+
+	/**
+	 * Schlüsselwortliste, Kommasepariert
+	 * Wird für den <meta name="keywords">-Tag verdet. Wird diese Zeile auskommentiert, wird kein solcher
+	 * <meta>-Tag generiert.
+	 */
+	'KEYWORDS' => 'smARTplaces, Video, Streaming, Live, Livestream',
+
+	/**
+	 * HTML-Code für den Footer (z.B. für spezielle Attribuierung mit <a>-Tags)
+	 * Sollte üblicherweise nur Inline-Elemente enthalten
+	 * Wird diese Zeile auskommentiert, wird die Standard-Attribuierung für (c3voc.de) verwendet
+	 */
+	'FOOTER_HTML' => '
+		by <a href="https://c3voc.de">C3VOC</a>
+	',
+
+	/**
+	 * HTML-Code für den Banner (nur auf der Startseite, direkt unter dem Header)
+	 * wird üblicherweise für KeyVisuals oder Textmarke verwendet (vgl. Blaues
+	 * Wischiwaschi auf http://media.ccc.de/)
+	 *
+	 * Dieser HTML-Block wird üblicherweise in der main.less speziell für die
+	 * Konferenz umgestaltet.
+	 *
+	 * Wird diese Zeile auskommentiert, wird kein Banner ausgegeben.
+	 */
+	 'BANNER_HTML' => '<div class="logo"></div>',
+
+	/**
+	 * Link zu den Recordings
+	 * Wird diese Zeile auskommentiert, wird der Link nicht angezeigt
+	 */
+	//'RELEASES' => 'https://youtube.com/...',
+
+	/**
+	 * Alternativ kann ein ReLive-Json konfiguriert werden, um die interne
+	 * ReLive-Ansicht zu aktivieren.
+	 *
+	 * Wird beides aktiviert, hat der externe Link Vorrang!
+	 * Wird beides auskommentiert, wird der Link nicht angezeigt
+	 */
+	 //'RELIVE_JSON' => 'http://live.alb.c3voc.de/relive/digitalitaet20/index.json',
+
+	/**
+	 * APCU-Cache-Zeit in Sekunden
+	 * Wird diese Zeile auskommentiert, werden die apc_*-Methoden nicht verwendet und
+	 * das Relive-Json bei jedem Request von der Quelle geladen und geparst
+	 */
+	//'RELIVE_JSON_CACHE' => 30*60,
+);
+
+/**
+ * Konfiguration der Stream-Übersicht auf der Startseite
+ */
+$CONFIG['OVERVIEW'] = array(
+	/**
+	 * Abschnitte aud der Startseite und darunter aufgeführte Räume
+	 * Es können beliebig neue Gruppen und Räume hinzugefügt werden
+	 *
+	 * Die Räume müssen in $CONFIG['ROOMS'] konfiguriert werden,
+	 * sonst werden sie nicht angezeigt.
+	 */
+	'GROUPS' => array(
+		'Lecture Rooms' => array(
+			'keynotes',
+			'sessionblock1',
+			'sessionblock2',
+			'sessionblock3',
+			'sessionblock4',
+			'sessionblock5',
+			'sessionblock6',
+			'sessionblock7',
+			'sessionblock8',
+		),
+	),
+);
+
+
+
+/**
+ * Liste der Räume (= Audio & Video Produktionen, also auch DJ-Sets oä.)
+ */
+$CONFIG['ROOMS'] = array(
+	'keynotes' => array(
+		'DISPLAY' => 'Keynotes',
+		'STREAM' => 'skeynotes',
+		'PREVIEW' => true,
+		'TRANSLATION' => true,
+		'SD_VIDEO' => true,
+		'HD_VIDEO' => true,
+		'DASH' => true,
+		'SLIDES' => false,
+		'AUDIO' => false,
+		'MUSIC' => false,
+		'SCHEDULE' => false,
+		'SCHEDULE_NAME' => 'Keynotes',
+		'FEEDBACK' => false,
+		'SUBTITLES' => false,
+		'EMBED' => true,
+		'IRC' => false,
+		'TWITTER' => false,
+	),
+
+	'sessionblock1' => array(
+		'DISPLAY' => 'Sessionblock 1',
+		'STREAM' => 'sessionblock1',
+		'PREVIEW' => true,
+		'TRANSLATION' => true,
+		'SD_VIDEO' => true,
+		'HD_VIDEO' => true,
+		'DASH' => true,
+		'SLIDES' => false,
+		'AUDIO' => false,
+		'MUSIC' => false,
+		'SCHEDULE' => false,
+		'SCHEDULE_NAME' => 'Sessionblock 1',
+		'FEEDBACK' => false,
+		'SUBTITLES' => false,
+		'EMBED' => true,
+		'IRC' => false,
+		'TWITTER' => false,
+	),
+
+	'sessionblock2' => array(
+		'DISPLAY' => 'Sessionblock 2',
+		'STREAM' => 'sessionblock2',
+		'PREVIEW' => true,
+		'TRANSLATION' => true,
+		'SD_VIDEO' => true,
+		'HD_VIDEO' => true,
+		'DASH' => true,
+		'SLIDES' => false,
+		'AUDIO' => false,
+		'MUSIC' => false,
+		'SCHEDULE' => false,
+		'SCHEDULE_NAME' => 'Sessionblock 2',
+		'FEEDBACK' => false,
+		'SUBTITLES' => false,
+		'EMBED' => true,
+		'IRC' => false,
+		'TWITTER' => false,
+	),
+
+	'sessionblock3' => array(
+		'DISPLAY' => 'Sessionblock 3',
+		'STREAM' => 'sessionblock3',
+		'PREVIEW' => true,
+		'TRANSLATION' => true,
+		'SD_VIDEO' => true,
+		'HD_VIDEO' => true,
+		'DASH' => true,
+		'SLIDES' => false,
+		'AUDIO' => false,
+		'MUSIC' => false,
+		'SCHEDULE' => false,
+		'SCHEDULE_NAME' => 'Sessionblock 3',
+		'FEEDBACK' => false,
+		'SUBTITLES' => false,
+		'EMBED' => true,
+		'IRC' => false,
+		'TWITTER' => false,
+	),
+
+	'sessionblock4' => array(
+		'DISPLAY' => 'Sessionblock 4',
+		'STREAM' => 'sessionblock4',
+		'PREVIEW' => true,
+		'TRANSLATION' => true,
+		'SD_VIDEO' => true,
+		'HD_VIDEO' => true,
+		'DASH' => true,
+		'SLIDES' => false,
+		'AUDIO' => false,
+		'MUSIC' => false,
+		'SCHEDULE' => false,
+		'SCHEDULE_NAME' => 'Sessionblock 4',
+		'FEEDBACK' => false,
+		'SUBTITLES' => false,
+		'EMBED' => true,
+		'IRC' => false,
+		'TWITTER' => false,
+	),
+
+	'sessionblock5' => array(
+		'DISPLAY' => 'Sessionblock 5',
+		'STREAM' => 'sessionblock5',
+		'PREVIEW' => true,
+		'TRANSLATION' => true,
+		'SD_VIDEO' => true,
+		'HD_VIDEO' => true,
+		'DASH' => true,
+		'SLIDES' => false,
+		'AUDIO' => false,
+		'MUSIC' => false,
+		'SCHEDULE' => false,
+		'SCHEDULE_NAME' => 'Sessionblock 5',
+		'FEEDBACK' => false,
+		'SUBTITLES' => false,
+		'EMBED' => true,
+		'IRC' => false,
+		'TWITTER' => false,
+	),
+
+	'sessionblock6' => array(
+		'DISPLAY' => 'Sessionblock 6',
+		'STREAM' => 'sessionblock6',
+		'PREVIEW' => true,
+		'TRANSLATION' => true,
+		'SD_VIDEO' => true,
+		'HD_VIDEO' => true,
+		'DASH' => true,
+		'SLIDES' => false,
+		'AUDIO' => false,
+		'MUSIC' => false,
+		'SCHEDULE' => false,
+		'SCHEDULE_NAME' => 'Sessionblock 6',
+		'FEEDBACK' => false,
+		'SUBTITLES' => false,
+		'EMBED' => true,
+		'IRC' => false,
+		'TWITTER' => false,
+	),
+
+	'sessionblock7' => array(
+		'DISPLAY' => 'Sessionblock 7',
+		'STREAM' => 'sessionblock7',
+		'PREVIEW' => true,
+		'TRANSLATION' => true,
+		'SD_VIDEO' => true,
+		'HD_VIDEO' => true,
+		'DASH' => true,
+		'SLIDES' => false,
+		'AUDIO' => false,
+		'MUSIC' => false,
+		'SCHEDULE' => false,
+		'SCHEDULE_NAME' => 'Sessionblock 7',
+		'FEEDBACK' => false,
+		'SUBTITLES' => false,
+		'EMBED' => true,
+		'IRC' => false,
+		'TWITTER' => false,
+	),
+
+	'sessionblock8' => array(
+		'DISPLAY' => 'Sessionblock 8',
+		'STREAM' => 'sessionblock8',
+		'PREVIEW' => true,
+		'TRANSLATION' => true,
+		'SD_VIDEO' => true,
+		'HD_VIDEO' => true,
+		'DASH' => true,
+		'SLIDES' => false,
+		'AUDIO' => false,
+		'MUSIC' => false,
+		'SCHEDULE' => false,
+		'SCHEDULE_NAME' => 'Sessionblock 8',
+		'FEEDBACK' => false,
+		'SUBTITLES' => false,
+		'EMBED' => true,
+		'IRC' => false,
+		'TWITTER' => false,
+	),
+
+);
+
+/**
+ * Globaler Schalter für die Embedding-Funktionalitäten
+ *
+ * Wird diese Zeile auskommentiert oder auf False gesetzt, werden alle
+ * Embedding-Funktionen deaktiviert.
+ */
+$CONFIG['EMBED'] = true;
+
+/**
+ * Konfigurationen zum Konferenz-Fahrplan
+ * Wird dieser Block auskommentiert, werden alle Fahrplan-Bezogenen Features deaktiviert
+ */
+$CONFIG['SCHEDULE'] = array(
+	/**
+	 * URL zum Fahrplan-XML
+	 *
+	 * Diese URL muss immer verfügbar sein, sonst können kann die Programm-Ansicht
+	 * aufhören zu funktionieren. Wenn die Quelle unverlässlich ist ;) sollte ein
+	 * externer HTTP-Cache vorgeschaltet werden.
+	 */
+	'URL' => '',
+
+	/**
+	* Nur die angegebenen Räume aus dem Fahrplan beachten
+	*
+	* Wird diese Zeile auskommentiert, werden alle Räume angezeigt
+	*/
+	//'ROOMFILTER' => [ '', ],
+
+	/**
+	 * Skalierung der Programm-Vorschau in Sekunden pro Pixel
+	 */
+	'SCALE' => 7,
+
+	/**
+	 * Simuliere das Verhalten als wäre die Konferenz bereits heute
+	 *
+	 * Diese folgende Beispiel-Zeile Simuliert, dass das
+	 * Konferenz-Datum 2014-12-29 auf den heutigen Tag 2015-02-24 verschoben ist.
+	 */
+	//'SIMULATE_OFFSET' => strtotime(/* Conference-Date */ '2016-05-21') - strtotime(/* Today */ '2016-05-19'),
+);
+
+/**
+ * Globale Konfiguration der Twitter-Links.
+ *
+ * Wird dieser Block auskommentiert, werden keine Twitter-Links mehr erzeugt. Sollen die
+ * Twitter-Links für jeden Raum einzeln konfiguriert werden, muss dieser Block trotzdem
+ * existieren sein. ggf. einfach auf true setzen:
+ *
+ *   $CONFIG['TWITTER'] = true
+ */
+$CONFIG['TWITTER'] = array(
+	/**
+	 * Anzeigetext für die Twitter-Links.
+	 *
+	 * %s wird durch den Raum-Slug ersetzt.
+	 * Ist eine weitere Anpassung erfoderlich, kann ein TWITTER_CONFIG-Block in der
+	 * Raum-Konfiguration zum Überschreiben dieser Angaben verwendet werden.
+	 */
+	'DISPLAY' => '#digitalitaet20 @ twitter',
+
+	/**
+	 * Vorgabe-Tweet-Text für die Twitter-Links.
+	 *
+	 * %s wird durch den Raum-Slug ersetzt.
+	 * Eine Anpassung kann ebenfalls in der Raum-Konfiguration vorgenommen werden.
+	 */
+	'TEXT' => '#digitalitaet20',
+);
+
+
+return $CONFIG;
