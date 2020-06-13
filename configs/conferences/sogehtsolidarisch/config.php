755401983fea4d3f8cf781bf305b9df713904486
Daniel Molkentin <daniel@molkentin.de>
2020-06-13 13:36:22 +0200

    sogentsolidatrisch: fix end date

--- configs/conferences/sogehtsolidarisch/config.php
+++ configs/conferences/sogehtsolidarisch/config.php
@@ -16,7 +16,7 @@ $CONFIG['CONFERENCE'] = array(
 	 *
 	 * Wird dieser Zeitpunkt nicht angegeben, endet die Konferenz nie. (Siehe aber CLOSED weiter unten)
 	 */
-	'ENDS_AT' => strtotime("2018-06-14 16:00"),
+	'ENDS_AT' => strtotime("2020-06-14 16:00"),
 
 	/**
 	 * Hiermit kann die Funktionalitaet von STARTS_AT/ENDS_AT überschrieben werden. Der Wert 'before'
