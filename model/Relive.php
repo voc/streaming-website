bb14de87d899b171966cfd4601e480f64f4eb037
Andreas Hubel <andi@saerdnaer.de>
2020-07-05 22:02:56 +0200

    sotm: change end date

--- model/Relive.php
+++ model/Relive.php
@@ -24,6 +24,11 @@ class Relive
 		return $this->getConference()->get('CONFERENCE.RELIVE_JSON');
 	}
 
+	public function getPreRecOffset()
+	{
+		return $this->getConference()->get('CONFERENCE.RELIVE_SKIP_PREREC_OFFSET') ?: 900;
+	}
+
 	public function getJsonCache()
 	{
 		return sprintf('/tmp/relive-cache-%s.json', $this->getConference()->getSlug());
