ab52e3eeede576d7d3ad530c8ed4a8dade15d7ca
Peter Hasse <peter.hasse@fokus.fraunhofer.de>
2020-09-29 18:48:24 +0200

    fixed typo in room name

--- configs/conferences/vcfb2020/config.php
+++ configs/conferences/vcfb2020/config.php
@@ -138,7 +138,7 @@ $CONFIG['ROOMS'] = array(
 	 * Array-Key ist der Raum-Slug, der z.B. auch zum erstellen der URLs,
 	 * in $CONFIG['OVERVIEW'] oder im Feedback verwendet wird.
 	 */
-	'Vortag' => array(
+	'Vortrag' => array(
 		/**
 		 * Angezeige-Name
 		 */
