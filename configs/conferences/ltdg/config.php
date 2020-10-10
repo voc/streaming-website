e15b1e914ca5e1c2b985ccaf33b1b44d6feb6afd
Daniel Molkentin <dmolkentin@suse.com>
2020-10-10 10:26:27 +0200

    Change Room Name

--- configs/conferences/ltdg/config.php
+++ configs/conferences/ltdg/config.php
@@ -148,7 +148,7 @@ $CONFIG['ROOMS'] = array(
 		 * Event-Case in welchem Raum aufgebaut wird und wird üblicherweise von
 		 * s1 bis s5 durchnummeriert.
 		 */
-		'STREAM' => 'ltdg1',
+		'STREAM' => 'ltdg-inout',
 
 		/**
 		 * Stream-Vorschaubildchen auf der Übersichtsseite anzeigen
