fcda39e7d245fe45ea1ff21bf048f69c6ca58adf
Andreas Hubel <andi@saerdnaer.de>
2020-10-23 23:03:05 +0200

    remove unneccesary download scripts

--- configs/conferences/pw20/download.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# fahrplan
-wget --no-check-certificate -q "https://cfp.privacyweek.at/pw20/schedule/export/schedule.xml" -O /tmp/pw20-schedule.xml && mv /tmp/pw20-schedule.xml schedule.xml
-
-# relive
-wget -q "http://live.dus.c3voc.de/relive/pw20/index.json" -O /tmp/vod.json && mv /tmp/vod.json vod.json
-rm -f /tmp/vod.json
