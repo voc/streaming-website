f79495b26423244a5772be35d6d79c3bae2eef1d
derpeter <sites@derpeter.net>
2021-02-24 20:22:58 +0100

    changed date for second demo day day

--- configs/conferences/pfdw21/config.php
+++ configs/conferences/pfdw21/config.php
@@ -8,7 +8,7 @@ $CONFIG['CONFERENCE'] = array(
 	 * Wird dieser Zeitpunkt nicht angegeben, gilt die Konferenz immer als angefangen. (Siehe aber ENDS_AT
 	 * und CLOSED weiter unten)
 	 */
-	'STARTS_AT' => strtotime("2021-02-22 18:15"),
+	'STARTS_AT' => strtotime("2021-02-26 17:15"),
 
 	/**
 	 * Der Endzeitpunkt der Konferenz als Unix-Timestamp. Befinden wir uns danach, wird eine Danke-Und-Kommen-Sie-
@@ -16,7 +16,7 @@ $CONFIG['CONFERENCE'] = array(
 	 *
 	 * Wird dieser Zeitpunkt nicht angegeben, endet die Konferenz nie. (Siehe aber CLOSED weiter unten)
 	 */
-	'ENDS_AT' => strtotime("2021-02-22 22:45"),
+	'ENDS_AT' => strtotime("2021-02-26 20:00"),
 
 	/**
 	 * Hiermit kann die Funktionalitaet von STARTS_AT/ENDS_AT überschrieben werden. Der Wert 'before'
