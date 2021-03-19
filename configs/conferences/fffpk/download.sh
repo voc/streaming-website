7de6fcd418988e3567aed299420a84da32a73cb4
Daniel Molkentin <daniel@molkentin.de>
2021-03-19 17:52:55 +0100

    Remove duplicate fffpk

--- configs/conferences/fffpk/download.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# fahrplan
-wget --no-check-certificate -q "https://cfp.privacyweek.at/pw18/schedule/export/schedule.xml" -O /tmp/pw18-schedule.xml && mv /tmp/pw18-schedule.xml schedule.xml
-
-# relive
-wget -q "http://live.dus.c3voc.de/relive/pw18/index.json" -O /tmp/vod.json && mv /tmp/vod.json vod.json
-rm -f /tmp/vod.json
