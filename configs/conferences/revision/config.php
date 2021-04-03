03b76f6ab1e90ace4a17733a2593bbdd00e0014b
Lukas Schauer <lukas@schauer.dev>
2021-04-03 21:37:20 +0200

    open revision

--- configs/conferences/revision/config.php
+++ configs/conferences/revision/config.php
@@ -26,7 +26,7 @@ $CONFIG['CONFERENCE'] = array(
 	 * Der Boolean true ist aus Abwärtskompatibilitätsgründen äquivalent zu 'after'. False ist äquivalent
 	 * zu 'running'.
 	 */
-	'CLOSED' => 'after',
+	'CLOSED' => 'running',
 
 	/**
 	 * Titel der Konferenz (kann Leer- und Sonderzeichen enthalten)
