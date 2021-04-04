57511e237db2ea189be9cd9397d5f5291307f424
Franziska Kunsmann <hi@kunsmann.eu>
2021-04-04 12:43:39 +0200

    serve.sh: try php7 first

--- serve.sh
+++ serve.sh
@@ -1,4 +1,22 @@
 #!/bin/sh
+
+php_bin=""
 port=8000
-if [ -n "$1" ]; then port=$1; fi
-php -S localhost:$port -d short_open_tag=true index.php
+
+for try_bin in {php7,php}
+do
+	php_bin=$(command -v $try_bin)
+	if [[ -n "$php_bin" ]]
+	then
+		break
+	fi
+done
+
+echo "Using PHP: $php_bin"
+
+if [ -n "$1" ]
+then
+	port=$1
+fi
+
+$php_bin -S localhost:$port -d short_open_tag=true index.php
