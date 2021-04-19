124a54b8cddbd19a86b3e44ef03fa93a7a1a2af1
Lukas Schauer <lukas@schauer.dev>
2021-04-19 21:47:52 +0200

    420 4 relive

--- configs/conferences/42021/config.php
+++ configs/conferences/42021/config.php
@@ -90,7 +90,7 @@ $CONFIG['CONFERENCE'] = array(
 	 * Wird beides aktiviert, hat der externe Link Vorrang!
 	 * Wird beides auskommentiert, wird der Link nicht angezeigt
 	 */
-	 //'RELIVE_JSON' => 'https://cdn.c3voc.de/relive/digitalitaet20/index.json',
+	 'RELIVE_JSON' => 'https://cdn.c3voc.de/relive/42021/index.json',
 
 	/**
 	 * APCU-Cache-Zeit in Sekunden
