1e5d93f4d3029353b7532452f47cb8f59b1329e9
Daniel Molkentin <daniel@molkentin.de>
2021-06-18 13:01:54 +0200

    fix relive url

--- configs/conferences/oSVC21/config.php
+++ configs/conferences/oSVC21/config.php
@@ -96,7 +96,7 @@ $CONFIG['CONFERENCE'] = array(
 	 * Wird beides aktiviert, hat der externe Link Vorrang!
 	 * Wird beides auskommentiert, wird der Link nicht angezeigt
 	 */
-	'RELIVE_JSON' => 'https://cdn.c3voc.de/relive/oSC21/index.json'
+	'RELIVE_JSON' => 'https://cdn.c3voc.de/relive/oSVC21/index.json'
 
 	/**
 	 * APCU-Cache-Zeit in Sekunden
