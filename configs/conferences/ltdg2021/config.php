d0238751b9c4a70e62ee03d25ba9502e1cdd2ab0
Daniel Molkentin <daniel@molkentin.de>
2021-10-08 18:38:49 +0200

    ltdg2021: no relive

--- configs/conferences/ltdg2021/config.php
+++ configs/conferences/ltdg2021/config.php
@@ -97,7 +97,7 @@ $CONFIG['CONFERENCE'] = array(
 	 * Wird beides aktiviert, hat der externe Link Vorrang!
 	 * Wird beides auskommentiert, wird der Link nicht angezeigt
 	 */
-	'RELIVE_JSON' => 'https://cdn.c3voc.de/relive/ltdg2021/index.json',
+	//'RELIVE_JSON' => 'https://cdn.c3voc.de/relive/ltdg2021/index.json',
 
 	/**
 	 * APCU-Cache-Zeit in Sekunden
