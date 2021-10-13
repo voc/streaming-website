fb2ec6657eb1a4094beaee98c9819d7606a88c3b
Lars Windauer <lars@existsolutions.com>
2021-10-14 01:23:48 +0200

    CADUS Debate #23 (fixed type in date)

--- configs/conferences/cadus-debate/config.php
+++ configs/conferences/cadus-debate/config.php
@@ -16,7 +16,7 @@ $CONFIG['CONFERENCE'] = array(
 	 *
 	 * Wird dieser Zeitpunkt nicht angegeben, endet die Konferenz nie. (Siehe aber CLOSED weiter unten)
 	 */
-	'ENDS_AT' => strtotime("2021-10-014 23:00"),
+	'ENDS_AT' => strtotime("2021-10-14 23:00"),
 
 	/**
 	 * Hiermit kann die Funktionalitaet von STARTS_AT/ENDS_AT überschrieben werden. Der Wert 'before'
