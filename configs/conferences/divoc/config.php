a6cfa54cac8c25b3050cf387caa14ff938615ee3
Jannik Beyerstedt <code@jannikbeyerstedt.de>
2022-04-10 20:08:14 +0200

    DiVOC BB3: Fix invalid config

--- configs/conferences/divoc/config.php
+++ configs/conferences/divoc/config.php
@@ -120,7 +120,7 @@ $CONFIG['OVERVIEW'] = array(
 	 */
 	'GROUPS' => array(
 		'Live' => array(
-			'stream',
+			'bb3',
 		),
 	),
 );
@@ -135,7 +135,7 @@ $CONFIG['ROOMS'] = array(
 	 * Array-Key ist der Raum-Slug, der z.B. auch zum erstellen der URLs,
 	 * in $CONFIG['OVERVIEW'] oder im Feedback verwendet wird.
 	 */
-	'stream' => array(
+	'bb3' => array(
 		'DISPLAY' => 'Main Stream',
 		'WIDE' => true,
 		'STREAM' => 'csh',
