1b20ded98390884a2395f3762dd1134eaa547a16
Lukas Schauer <lukas@schauer.dev>
2022-04-28 21:15:08 +0200

    bba2022: some fixes

--- configs/conferences/bba2022/config.php
+++ configs/conferences/bba2022/config.php
@@ -134,7 +134,7 @@ $CONFIG['ROOMS'] = array(
 	 * Array-Key ist der Raum-Slug, der z.B. auch zum erstellen der URLs,
 	 * in $CONFIG['OVERVIEW'] oder im Feedback verwendet wird.
 	 */
-	'bba21' => array(
+	'bba22' => array(
 		/**
 		 * Angezeige-Name
 		 */
