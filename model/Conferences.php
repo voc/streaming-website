59e8e25927ec3bb84d1ed4b3957beaa146a0a2a2
Andreas Hubel <andi@saerdnaer.de>
2022-05-13 14:29:57 +0200

    catch config not found exception as 404

--- model/Conferences.php
+++ model/Conferences.php
@@ -101,7 +101,13 @@ class Conferences
 
 	public static function loadConferenceConfig($mandator) {
 		$configfile = forceslash(Conferences::MANDATOR_DIR).forceslash($mandator).'config.php';
-		$config = include($configfile);
+
+		try {
+			$config = include($configfile);
+		}
+		catch(Exception $e) {
+			throw new NotFoundException();
+		}
 
 		if(!is_array($config)) {
 			throw new ConfigException("Loading $configfile did not return an array. Maybe it's missing a return-statement?");
