4da8126eb843b44ba639a2ba4d85e4b728df3756
Andreas Hubel <andi@saerdnaer.de>
2022-05-15 16:01:00 +0200

    use new flag for deploy script
    so we do not need php / docker on the jenkins host

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" != '--without-validation' ]; then
 	for cmd in find xargs php git; do
-		command -v $cmd >/dev/null 2>&1 || { echo >&2 "I require $cmd but it's not installed.  Aborting."; exit 1; }
+		command -v "$cmd" >/dev/null 2>&1 || { echo >&2 "I require $cmd but it's not installed.  Aborting."; exit 1; }
 	done
 
 	find . -name "*.php" | grep -v archive | xargs -n1 php -l
@@ -13,9 +13,13 @@ if [ "$1" != '--without-validation' ]; then
 fi
 
 echo ""
-DEPLOY_BRANCH=`git rev-parse --abbrev-ref HEAD`
+DEPLOY_BRANCH=`git rev-parse --abbrev-ref HEAD` || "$BRANCH_NAME" 
 
-if [ `git rev-parse --verify origin/$DEPLOY_BRANCH` != `git rev-parse --verify $DEPLOY_BRANCH` ] && [ `git rev-parse --verify origin/staging` != `git rev-parse --verify $DEPLOY_BRANCH` ]; then
+echo "On are trying to deploy branch: $DEPLOY_BRANCH…"
+echo ""
+
+if [ `git rev-parse --verify origin/$DEPLOY_BRANCH` != `git rev-parse --verify "$DEPLOY_BRANCH"` ] \
+&& [ `git rev-parse --verify origin/staging` != `git rev-parse --verify "$DEPLOY_BRANCH"` ]; then
 	echo "You have commits on the $DEPLOY_BRANCH branch not pushed to origin yet. They would not be deployed."
 	echo "do you still which to deploy what's already in the repo? then type yes"
 	read -p "" input
@@ -28,16 +32,6 @@ fi
 if ! (git diff --exit-code >/dev/null && git diff --cached --exit-code >/dev/null); then
 	echo "You have uncomitted changes. They would not be deployed."
 	echo "do you still which to deploy what's already in the repo? then type yes"
-	read -p "" input
-	if [ "x$input" != "xyes" ]; then
-		exit 2
-	fi
-	echo ""
-fi
-
-if [ "x$DEPLOY_BRANCH" != "xmaster" ] && [ "x$DEPLOY_BRANCH" != "xstaging" ]; then
-	echo "You're currently on branch $DEPLOY_BRANCH."
-	echo "Are you sure you want to deploy that branch (and not master)? then type yes"
 	read -p "" input
 	if [ "x$input" != "xyes" ]; then
 		exit 2
