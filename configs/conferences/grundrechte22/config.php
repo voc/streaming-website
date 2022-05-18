b3936e78d98150659cbb3b8c39858301b32be76e
Daniel Molkentin <daniel@molkentin.de>
2022-05-18 09:49:05 +0200

    grundrechte: fix time stamp

--- configs/conferences/grundrechte22/config.php
+++ configs/conferences/grundrechte22/config.php
@@ -8,7 +8,7 @@ $CONFIG['CONFERENCE'] = array(
 	 * Wird dieser Zeitpunkt nicht angegeben, gilt die Konferenz immer als angefangen. (Siehe aber ENDS_AT
 	 * und CLOSED weiter unten)
 	 */
-	'STARTS_AT' => strtotime("2022-05-18 9:45"),
+	'STARTS_AT' => strtotime("2022-05-18 09:45"),
 
 	/**
 	 * Der Endzeitpunkt der Konferenz als Unix-Timestamp. Befinden wir uns danach, wird eine Danke-Und-Kommen-Sie-
