ee045b91c08c4814ea5f97104ceae7beda937345
Ember Keske <git@n0emis.eu>
2022-10-01 10:19:22 +0200

    update relive for bub

--- configs/conferences/bub2022/config.php
+++ configs/conferences/bub2022/config.php
@@ -96,7 +96,7 @@ $CONFIG['CONFERENCE'] = array(
 	 * Wird beides aktiviert, hat der externe Link Vorrang!
 	 * Wird beides auskommentiert, wird der Link nicht angezeigt
 	 */
-	'RELIVE_JSON' => 'https://cdn.c3voc.de/relive/bub2022/index.json',
+	'RELIVE_JSON' => 'https://cdn.c3voc.de/relive/bitsundbaeume/index.json',
 
 	/**
 	 * APCU-Cache-Zeit in Sekundenconfigs/conferences/bornhack22/config.php
