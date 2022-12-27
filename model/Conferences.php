voc/streaming-website

commit 36a6ee344fe1332ce90eafef727e0a45a4eee0f9

misc fixes

model/Conferences.php
@@ -132,6 +132,11 @@ class Conferences
 			return new Conference($config, $mandator);
 		}
 
+		// config option for dynamic lookup feature defined below
+		if (!@$GLOBALS['CONFIG']['DYNAMIC_LOOKUP']) {
+			return false;
+		}
+
 		// otherwise try to find conference in c3data postgres
 		$query = 'query StreamingConfig($acronym: String!) {
 			conference(acronym: $acronym) {

config.php
@@ -38,6 +38,12 @@ else
 	$GLOBALS['CONFIG']['BASEURL'] = '//streaming.media.ccc.de/';
 }
 
+/**
+ * Wenn die Konfiguration einer Konferenz/Mandant nicht im Dateisystem gefunden wird:
+ * Soll die Streamingwebseite versuchen diese bei data.c3voc.de per API abzufragen?
+ */
+$GLOBALS['CONFIG']['DYNAMIC_LOOKUP'] = false;
+
 
 /**
  * Konfiguration für den Datei-Download Cronjob
@@ -103,7 +109,10 @@ $GLOBALS['CONFIG']['FEEDBACK'] = array(
  */
 
 $GLOBALS['CONFIG']['ROOM_DEFAULTS'] = array(
-	'WIDE' => true,
+	/**
+	 * Breite Teaser in der Übersichtsseite
+	 */
+	'WIDE' => false,
 
 	/**
 	 * Stream-Vorschaubildchen auf der Übersichtsseite anzeigen

index.php
@@ -205,9 +205,9 @@ try {
 		'conference_assets' => forceslash($mandator),
 
 		'conference' => $conference,
-		'feedback' => $conference->getFeedback(),
-		'schedule' => $conference->getSchedule(),
-		'subtitles' => $conference->getSubtitles(),
+		'feedback' => $conference ? $conference->getFeedback() : false,
+		'schedule' => $conference ? $conference->getSchedule() : false,
+		'subtitles' => $conference ? $conference->getSubtitles() : false,
 	));
 
 	// ALWAYS AVAILABLE ROUTES

model/ConferenceJson.php
@@ -15,7 +15,7 @@ class ConferenceJson extends Conference
 		$this->html  = @$c->streamingConfig->html ?: [];
 
 		$this->rooms = [];
-		$rooms = is_array(@$c->rooms) ? $c->rooms : @$c->rooms->nodes;
+		$rooms = (is_array(@$c->rooms) ? $c->rooms : @$c->rooms->nodes) ?: [];
 		foreach($rooms as $r) {
 			if (!$r) {
 				continue;

view/config-json.php
@@ -53,15 +53,17 @@ function formatRooms($conference) {
 		unset($config['guid']);
 		unset($config['name']);
 		unset($config['slug']);
-		foreach ($config['chat'] as $k => $v) {
-			unset($config[$k]);
+		if (isset($config['chat'])) {
+			foreach ($config['chat'] as $k => $v) {
+				unset($config[$k]);
+			}
 		}
 
 
 		$struct[] = array(
 			'guid' => $room->getId(),
 			'slug' => $room->getSlug(),
-			'name' => $room->get('name') ?: $room->getScheduleName(),
+			'name' => /*$room->get('name') ?: */ $room->getScheduleName(),
 			'stream' => $room->getStream(),
 			'streamingConfig' => $config ? lowerCaseKeys($config) : null,
 		);

commit f7c98b966317387f6f881138c0b66f9a6a22b164

fix confernece resolution issue introduced by previous commit
This fixes an bug introduced with commit 36a6ee344fe1332ce90eafef727e0a45a4eee0f9.

model/Conferences.php
@@ -134,37 +134,44 @@ class Conferences
 
 		// config option for dynamic lookup feature defined below
 		if (!@$GLOBALS['CONFIG']['DYNAMIC_LOOKUP']) {
-			return false;
+			throw new NotFoundException();;
 		}
 
-		// otherwise try to find conference in c3data postgres
-		$query = 'query StreamingConfig($acronym: String!) {
-			conference(acronym: $acronym) {
-				title
-				acronym
-				description
-				keywords
-				organizer
-				start: startDate
-				end: endDate
-				streamingConfig 
-				
-				rooms(orderBy: [RANK_ASC, NAME_ASC]' . ( 
-					true ? ', filter: {streamId: {isNull: false}}' : '' 
-				) . ' ) {
-					nodes {
-						guid
-						name
-						slug
-						streamId
-						streamingConfig
+		try {
+			// otherwise try to find conference in c3data postgres
+			$query = 'query StreamingConfig($acronym: String!) {
+				conference(acronym: $acronym) {
+					title
+					acronym
+					description
+					keywords
+					organizer
+					start: startDate
+					end: endDate
+					streamingConfig 
+					
+					rooms(orderBy: [RANK_ASC, NAME_ASC]' . ( 
+						true ? ', filter: {streamId: {isNull: false}}' : '' 
+					) . ' ) {
+						nodes {
+							guid
+							name
+							slug
+							streamId
+							streamingConfig
+						}
 					}
 				}
-			}
-		}';
-		$data = query_data('conferenceConfig', $query, ['acronym' => $mandator]);
+			}';
+			$data = query_data('conferenceConfig', $query, ['acronym' => $mandator]);
 
-		return new ConferenceJson($data, $mandator);
+			return new ConferenceJson($data, $mandator);
+		}
+		catch(Exception $e) {
+			throw new NotFoundException();
+		}
+
+		
 	}
 
 	public static function getConference($mandator) {