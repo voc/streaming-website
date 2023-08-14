1acebc4cae6c229a48fd085136c2a01b87cd1155
1uk4sh <1uk4sh@posteo.de>
2023-08-14 17:53:20 +0200

    cccamp2023: fix starttime

--- configs/conferences/camp2023/config.php
+++ configs/conferences/camp2023/config.php
@@ -8,7 +8,7 @@ $CONFIG['CONFERENCE'] = array(
 	 * Wird dieser Zeitpunkt nicht angegeben, gilt die Konferenz immer als angefangen. (Siehe aber ENDS_AT
 	 * und CLOSED weiter unten)
 	 */
-	'STARTS_AT' => strtotime("2023-08-15 10:00"),
+	'STARTS_AT' => strtotime("2023-08-14 19:00"),
 
 	/**
 	 * Der Endzeitpunkt der Konferenz als Unix-Timestamp. Befinden wir uns danach, wird eine Danke-Und-Kommen-Sie-
