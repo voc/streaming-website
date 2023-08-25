333abb0b37ab98de8d8636c2099008936f91095d
Franziska Kunsmann <hi@kunsmann.eu>
2023-08-25 06:51:56 +0200

    fix more clubsareculture config

--- configs/conferences/clubsareculture23/config.php
+++ configs/conferences/clubsareculture23/config.php
@@ -154,8 +154,8 @@ $CONFIG['ROOMS'] = array(
 	 * Array-Key ist der Raum-Slug, der z.B. auch zum erstellen der URLs,
 	 * in $CONFIG['OVERVIEW'] oder im Feedback verwendet wird.
 	 */
-	'clubsareculture' => array(
-		'DISPLAY' => 'momem',
+	'momem' => array(
+		'DISPLAY' => 'MOMEM',
 		'STREAM' => 'clubsareculture',
 		'PREVIEW' => true,
 		'TRANSLATION' => false,
