72f903174a8e0004e2953a9b32608a2e14d4c11d
Daniel Molkentin <daniel@molkentin.de>
2023-09-12 20:39:14 +0200

    asg2023: fix end date

--- configs/conferences/asg2023/config.php
+++ configs/conferences/asg2023/config.php
@@ -16,7 +16,7 @@ $CONFIG['CONFERENCE'] = array(
 	 *
 	 * Wird dieser Zeitpunkt nicht angegeben, endet die Konferenz nie. (Siehe aber CLOSED weiter unten)
 	 */
-	'ENDS_AT' => strtotime("2019-09-14 18:30"),
+	'ENDS_AT' => strtotime("2023-09-14 18:30"),
 
 	/**
 	 * Hiermit kann die Funktionalitaet von STARTS_AT/ENDS_AT überschrieben werden. Der Wert 'before'
