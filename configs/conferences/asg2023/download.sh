cde17049b8ce0cd7fd4c55699f5e091ea030bcc5
Daniel Molkentin <daniel@molkentin.de>
2023-09-12 20:35:44 +0200

    asg2023: remove useless download.sh

--- configs/conferences/asg2023/download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# fahrplan
-wget --no-check-certificate -q "https://conf.qtcon.org/en/qtcon/public/schedule.xml" -O /tmp/qtcon16-schedule.xml && mv /tmp/qtcon16-schedule.xml schedule.xml
-rm -f /tmp/qtcon16-schedule.xml
-
-# relive
-wget -q "http://live.dus.c3voc.de/relive/qtcon16/index.json" -O /tmp/qtcon16-vod.json && mv /tmp/qtcon16-vod.json vod.json
-rm -f /tmp/qtcon16-vod.json
