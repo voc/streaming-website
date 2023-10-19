133dcd82d455536a1916f189ccf1771a1cd24d2f
Franziska Kunsmann <hi@kunsmann.eu>
2023-10-19 12:45:00 +0200

    jh23ch: fix stream name and schedule

--- configs/conferences/jh23ch/config.php
+++ configs/conferences/jh23ch/config.php
@@ -146,7 +146,7 @@ $CONFIG['ROOMS'] = array(
 		 * Event-Case in welchem Raum aufgebaut wird und wird üblicherweise von
 		 * s1 bis s5 durchnummeriert.
 		 */
-		'STREAM' => 's1',
+		'STREAM' => 'jugendhackt',
 
 		/**
 		 * Stream-Vorschaubildchen auf der Übersichtsseite anzeigen
@@ -235,7 +235,7 @@ $CONFIG['ROOMS'] = array(
 		 * Ebenso können alle Fahrplan-Funktionialitäten durch auskommentieren
 		 * des globalen $CONFIG['SCHEDULE']-Blocks deaktiviert werden
 		 */
-		'SCHEDULE' => true,
+		'SCHEDULE' => false,
 
 		/**
 		 * Name des Raums im Fahrplan
