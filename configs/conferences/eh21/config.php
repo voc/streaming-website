5a01bc49141ec7b90f90979e362bad9848f77316
Sophie Schiller <sophie.schiller01@gmail.com>
2024-03-29 15:46:58 +0100

    eh21: end date is important

--- configs/conferences/eh21/config.php
+++ configs/conferences/eh21/config.php
@@ -16,7 +16,7 @@ $CONFIG['CONFERENCE'] = array(
 	 *
 	 * Wird dieser Zeitpunkt nicht angegeben, endet die Konferenz nie. (Siehe aber CLOSED weiter unten)
 	 */
-	'ENDS_AT' => strtotime("2023-04-01 16:00"),
+	'ENDS_AT' => strtotime("2024-04-01 16:00"),
 
 	/**
 	 * Hiermit kann die Funktionalitaet von STARTS_AT/ENDS_AT überschrieben werden. Der Wert 'before'
