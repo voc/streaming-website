83dd543d0ee34651ac1711b7ee1463831fadaaa2
Jan Koppe <post@jankoppe.de>
2024-05-08 19:24:11 +0200

    kamphack2024: adjust stream name

--- configs/conferences/kamphack2024/config.php
+++ configs/conferences/kamphack2024/config.php
@@ -146,7 +146,7 @@ $CONFIG['ROOMS'] = array(
 		 * Event-Case in welchem Raum aufgebaut wird und wird üblicherweise von
 		 * s1 bis s5 durchnummeriert.
 		 */
-		'STREAM' => 'kamphack24',
+		'STREAM' => 'kamphack',
 
 		/**
 		 * Stream-Vorschaubildchen auf der Übersichtsseite anzeigen
