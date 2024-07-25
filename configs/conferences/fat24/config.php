02d9b2696297ac2d5483c313a7a26f601a8908c4
L0op <57923697+xL0opx@users.noreply.github.com>
2024-07-25 17:12:46 +0200

    Add config for fat24

---
+++ configs/conferences/fat24/config.php
@@ -0,0 +1,233 @@
+<?php
+
+$CONFIG['CONFERENCE'] = array(
+	/**
+	 * Der Startzeitpunkt der Konferenz als Unix-Timestamp. Befinden wir uns davor, wird die Closed-Seite
+	 * mit einem Text der Art "hat noch nicht angefangen" angezeigt.
+	 *
+	 * Wird dieser Zeitpunkt nicht angegeben, gilt die Konferenz immer als angefangen. (Siehe aber ENDS_AT
+	 * und CLOSED weiter unten)
+	 */
+	'STARTS_AT' => strtotime("2024-07-26 16:00 UTC+02:00"),
+
+	/**
+	 * Der Endzeitpunkt der Konferenz als Unix-Timestamp. Befinden wir uns danach, wird eine Danke-Und-Kommen-Sie-
+	 * Gut-Nach-Hause-Seite sowie einem Ausblick auf die kommenden Events angezeigt.
+	 *
+	 * Wird dieser Zeitpunkt nicht angegeben, endet die Konferenz nie. (Siehe aber CLOSED weiter unten)
+	 */
+	'ENDS_AT' => strtotime("2024-07-28 18:00 UTC+02:00"),
+
+	/**
+	 * Hiermit kann die Funktionalitaet von STARTS_AT/ENDS_AT überschrieben werden. Der Wert 'before'
+	 * simuliert, dass die Konferenz noch nicht begonnen hat. Der Wert 'after' simuliert, dass die Konferenz
+	 * bereits beendet ist. 'running' simuliert eine laufende Konferenz.
+	 *
+	 * Der Boolean true ist aus Abwärtskompatibilitätsgründen äquivalent zu 'after'. False ist äquivalent
+	 * zu 'running'.
+	 */
+	//'CLOSED' => 'running',
+
+	/**
+	 * Titel der Konferenz (kann Leer- und Sonderzeichen enthalten)
+	 * Dieser im Seiten-Header, im <title>-Tag, in der About-Seite und ggf. ab weiteren Stellen als
+	 * Anzeigetext benutzt
+	 */
+	'TITLE' => 'FAT24',
+
+	/**
+	 * Veranstalter
+	 * Wird für den <meta name="author">-Tag verdet. Wird diese Zeile auskommentiert, wird kein solcher
+	 * <meta>-Tag generiert.
+	 */
+	'AUTHOR' => 'Hackwerk Aalen',
+
+	/**
+	 * Beschreibungstext
+	 * Wird für den <meta name="description">-Tag verdet. Wird diese Zeile auskommentiert, wird kein solcher
+	 * <meta>-Tag generiert.
+	 */
+	'DESCRIPTION' => 'Das Hackwerk Aalen veranstaltet vom 26.07.24-28.07.24 in Aalen, auf der schönen Ostalb die FAT24 mit dem Motto „3 Tage 3 Byte“. Diese führt die „Geekends“-Veranstaltungen (2022 und 2023) weiter und ist damit das 3. größere Event des Hackwerk Aalens. Geplant sind drei spannende Tage, die vor allem für spannende Vorträge und Gespräche mit alten und neuen Wesen aus der Chaos-Welt geführt werden können. Natürlich gibt es auch wieder ein großes Hackcenter.',
+	
+
+	/**
+	 * Schlüsselwortliste, Kommasepariert
+	 * Wird für den <meta name="keywords">-Tag verdet. Wird diese Zeile auskommentiert, wird kein solcher
+	 * <meta>-Tag generiert.
+	 */
+	'KEYWORDS' => 'Aalen, FAT24, Hackwerk',
+
+	/**
+	 * HTML-Code für den Footer (z.B. für spezielle Attribuierung mit <a>-Tags)
+	 * Sollte üblicherweise nur Inline-Elemente enthalten
+	 * Wird diese Zeile auskommentiert, wird die Standard-Attribuierung für (c3voc.de) verwendet
+	 */
+	'FOOTER_HTML' => '
+		by <a href="https://aalen.space/">Hackwerk Aalen</a>
+	',
+
+	/**
+	 * HTML-Code für den Banner (nur auf der Startseite, direkt unter dem Header)
+	 * wird üblicherweise für KeyVisuals oder Textmarke verwendet (vgl. Blaues
+	 * Wischiwaschi auf http://media.ccc.de/)
+	 *
+	 * Dieser HTML-Block wird üblicherweise in der main.less speziell für die
+	 * Konferenz umgestaltet.
+	 *
+	 * Wird diese Zeile auskommentiert, wird kein Banner ausgegeben.
+	 */
+	'BANNER_HTML' => '<img src=fat_logo_banner_wide.png>',
+
+	/**
+	 * Link zu den Recordings
+	 * Wird diese Zeile auskommentiert, wird der Link nicht angezeigt
+	 */
+	//'RELEASES' => 'https://media.ccc.de/b/conferences/geekend-aalen',
+
+	/**
+	 * Link zu einer (externen) ReLive-Übersichts-Seite
+	 * Wird diese Zeile auskommentiert, wird der Link nicht angezeigt
+	 */
+	//'RELIVE' => 'http://vod.c3voc.de/',
+
+	/**
+	 * Alternativ kann ein ReLive-Json konfiguriert werden, um die interne
+	 * ReLive-Ansicht zu aktivieren.
+	 *
+	 * Wird beides aktiviert, hat der externe Link Vorrang!
+	 * Wird beides auskommentiert, wird der Link nicht angezeigt
+	 */
+	//'RELIVE_JSON' => 'https://cdn.c3voc.de/relive/fiffkon22/index.json',
+
+	/**
+	 * APCU-Cache-Zeit in Sekunden
+	 * Wird diese Zeile auskommentiert, werden die apc_*-Methoden nicht verwendet und
+	 * das Relive-Json bei jedem Request von der Quelle geladen und geparst
+	 */
+	//'RELIVE_JSON_CACHE' => 30*60,
+);
+
+/**
+ * Konfiguration der Stream-Übersicht auf der Startseite
+ */
+$CONFIG['OVERVIEW'] = array(
+	/**
+	 * Abschnitte aud der Startseite und darunter aufgeführte Räume
+	 * Es können beliebig neue Gruppen und Räume hinzugefügt werden
+	 *
+	 * Die Räume müssen in $CONFIG['ROOMS'] konfiguriert werden,
+	 * sonst werden sie nicht angezeigt.
+	 */
+	'GROUPS' => array(
+		'Lecture Rooms' => array(
+			'Vortragsraum',
+		),
+	),
+);
+
+
+
+/**
+ * Liste der Räume (= Audio & Video Produktionen, also auch DJ-Sets oä.)
+ */
+$CONFIG['ROOMS'] = array(
+	'Vortragsraum' => array(
+		'DISPLAY' => 'Vortragsraum',
+		'STREAM' => 'aalend',
+		'PREVIEW' => true,
+		'TRANSLATION' => false,
+		'SD_VIDEO' => true,
+		'HD_VIDEO' => true,
+		'DASH' => true,
+		'H264_ONLY' => true,
+		'HLS' => true,
+		'SLIDES' => false,
+		'AUDIO' => false,
+		'MUSIC' => false,
+		'SCHEDULE' => true,
+		'SCHEDULE_NAME' => 'Vortragsraum',
+		'FEEDBACK' => false,
+		'SUBTITLES' => false,
+		'EMBED' => true,
+		'IRC' => false,
+		'TWITTER' => false,
+	),
+);
+
+/**
+ * Globaler Schalter für die Embedding-Funktionalitäten
+ *
+ * Wird diese Zeile auskommentiert oder auf False gesetzt, werden alle
+ * Embedding-Funktionen deaktiviert.
+ */
+$CONFIG['EMBED'] = true;
+
+/**
+ * Konfigurationen zum Konferenz-Fahrplan
+ * Wird dieser Block auskommentiert, werden alle Fahrplan-Bezogenen Features deaktiviert
+ */
+$CONFIG['SCHEDULE'] = array(
+	/**
+	 * URL zum Fahrplan-XML
+	 *
+	 * Diese URL muss immer verfügbar sein, sonst können kann die Programm-Ansicht
+	 * aufhören zu funktionieren. Wenn die Quelle unverlässlich ist ;) sollte ein
+	 * externer HTTP-Cache vorgeschaltet werden.
+	 */
+	'URL' => 'https://participate.hackwerk.fun/fat24/schedule/export/schedule.xml',
+
+	/**
+	* Nur die angegebenen Räume aus dem Fahrplan beachten
+	*
+	* Wird diese Zeile auskommentiert, werden alle Räume angezeigt
+	*/
+	'ROOMFILTER' => [
+		'Vortragsraum',
+	],
+
+	/**
+	 * Skalierung der Programm-Vorschau in Sekunden pro Pixel
+	 */
+	'SCALE' => 7,
+
+	/**
+	 * Simuliere das Verhalten als wäre die Konferenz bereits heute
+	 *
+	 * Diese folgende Beispiel-Zeile Simuliert, dass das
+	 * Konferenz-Datum 2014-12-29 auf den heutigen Tag 2015-02-24 verschoben ist.
+	 */
+	//'SIMULATE_OFFSET' => strtotime(/* Conference-Date */ '2016-05-21') - strtotime(/* Today */ '2016-05-19'),
+	'SIMULATE_OFFSET' => strtotime(/* Conference-Date */ '2024-07-27') - strtotime(/* Today */ '2024-07-25'),
+);
+
+/**
+ * Globale Konfiguration der Twitter-Links.
+ *
+ * Wird dieser Block auskommentiert, werden keine Twitter-Links mehr erzeugt. Sollen die
+ * Twitter-Links für jeden Raum einzeln konfiguriert werden, muss dieser Block trotzdem
+ * existieren sein. ggf. einfach auf true setzen:
+ *
+ *   $CONFIG['TWITTER'] = true
+ */
+//$CONFIG['TWITTER'] = array(
+	/**
+	 * Anzeigetext für die Twitter-Links.
+	 *
+	 * %s wird durch den Raum-Slug ersetzt.
+	 * Ist eine weitere Anpassung erfoderlich, kann ein TWITTER_CONFIG-Block in der
+	 * Raum-Konfiguration zum Überschreiben dieser Angaben verwendet werden.
+	 */
+	//'DISPLAY' => '#vcfb2022 @vcfberlin on twitter',
+
+	/**
+	 * Vorgabe-Tweet-Text für die Twitter-Links.
+	 *
+	 * %s wird durch den Raum-Slug ersetzt.
+	 * Eine Anpassung kann ebenfalls in der Raum-Konfiguration vorgenommen werden.
+	 */
+	//'TEXT' => '#vcfb2022',
+//);
+
+
+return $CONFIG;
+?>
